a49eecccec5382e6892bad8adef883f6bb1e5a60
Mikhail B <diskauntm@gmail.com>
2025-03-24 16:58:11 +0300

    modify next config

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
   distDir: 'dist',
   images: {},
-	eslint: {
+  eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
     // ignoreDuringBuilds: true,
   },
-	assetPrefix: './public',
+  basePath: isProd ? '/monolith-frame' : '',
 };
 module.exports = nextConfig;
